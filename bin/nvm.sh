cc3cc79426ca9ce87d4647a9a138e8a0e163949f
Joel Chen <joel123@gmail.com>
2025-10-26 00:00:13 -0700

    Fix nvm.sh sourcing to work correctly when sourced

--- bin/nvm.sh
+++ bin/nvm.sh
@@ -1,5 +1,15 @@
 # Backward compatibility alias - nvm command points to unvm
 # This file provides the nvm alias for users upgrading from older versions
 
+# Get the directory of this script (works when sourced)
+if [ -n "$BASH_SOURCE" ]; then
+  NVM_SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+elif [ -n "$ZSH_VERSION" ]; then
+  NVM_SCRIPT_DIR="$(cd "$(dirname "${(%):-%x}")" && pwd)"
+else
+  NVM_SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+fi
+
 # Source the main unvm.sh file
-source "$(dirname "$0")/unvm.sh"
+source "${NVM_SCRIPT_DIR}/unvm.sh"
+unset NVM_SCRIPT_DIR
